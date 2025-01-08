cfe371bfb7e102c89fa11c921fd45323465abb92
Abhishek <abhishekpawar2021.ainds@mmcoe.edu.in>
2025-01-08 22:03:38 +0530

    updated codebase

--- Data/datasetPrep.py
+++ Data/datasetPrep.py
@@ -2,14 +2,14 @@ import random
 import pandas as pd
 from datetime import datetime, timedelta
 
-def generateDate():
+def getDate():
     end_date = datetime.now()
     start_date = end_date - timedelta(days=60)  # Two months ago
     random_date = start_date + (end_date - start_date) * random.random()
     return random_date
 
 
-def generate_post():
+def getPost():
     post_types = ['Image', 'Video', 'Reel', 'Carousel']
 
     post_type = random.choice(post_types)
@@ -21,14 +21,14 @@ def generate_post():
     impressions = random.randint(1000, 15000)
     reach = random.randint(1000, 12000)
 
-    posted_at = generateDate()
+    posted_at = getDate()
 
     return [post_type, likes, comments, shares, impressions, reach, save_count, posted_at]
 
-def generate_instagram_data(num_posts=100):
+def generateDataset(size=100):
     data = []
-    for _ in range(num_posts):
-        data.append(generate_post())
+    for _ in range(size):
+        data.append(getPost())
     
     columns = ['Post Type', 'Likes', 'Comments', 'Shares', 'Impressions', 'Reach', 'Save Count', 'Posted At']
 
@@ -36,6 +36,6 @@ def generate_instagram_data(num_posts=100):
     
     return df
 
-instagram_data = generate_instagram_data(num_posts=100)
+instagram_data = generateDataset(size=100)
 
 instagram_data.to_csv('mock_instagram_data.csv', index=False)
